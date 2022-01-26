1e841505aaff6fb959c956164e9cc344f7994eb5
Pier4413 <pierre.delmas@delmasweb.net>
2022-01-26 20:27:23 +0100

    Suppression informations inutiles

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String args[])  //static method  
     {  
-        new MainFrame();
+        
     } 
 
 }
--- src/main/java/Screen.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JLabel;
-
-public class Screen extends JLabel {
-
-    public Screen(int x, int y, int width, int height) {
-        this.setBounds(x, y, width, height);
-        
-    }
-}
