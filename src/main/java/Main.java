390054b5dac0d87825d4158d124105200ddca0e1
Delmas19 <85925571+Delmas19@users.noreply.github.com>
2022-02-09 18:19:25 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String args[])  //static method  
     {  
+        Chaise mSuperChaise = new Chaise(4, true,1, true);
         
+        System.out.println(maSuperChaise.getNombreDePieds());
     } 
 
 }
